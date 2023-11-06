0f9c36975061b7ad82faadecf01e0407e8266691
Daniil <daniilminin1990@yandex.ru>
2023-11-07 01:00:41 +0600

    header in the first approximation
    refactoring heaeder, now ot looks more usual

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,30 +1,19 @@
 import React from "react";
+import styled from "styled-components";
+import { Logo } from "../../components/logo/Logo";
+import { Menu } from "../../components/menu/Menu";
 
 export const Header = () => {
   return (
-    <header>
-      <a href="#">
-        <img src="" alt="" />
-      </a>
-      <nav>
-        <ul>
-          <li>
-            <a href="">Home</a>
-          </li>
-          <li>
-            <a href="">Skills</a>
-          </li>
-          <li>
-            <a href="">Works</a>
-          </li>
-          <li>
-            <a href="">Testimony</a>
-          </li>
-          <li>
-            <a href="">Contact</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <StyledHeader>
+      <Logo />
+      <Menu />
+    </StyledHeader>
   );
 };
+
+const StyledHeader = styled.header`
+  background-color: #d4ffd3;
+  display: flex;
+  justify-content: space-between;
+`;
