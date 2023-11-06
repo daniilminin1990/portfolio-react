53dd5ce542f8aefaa5f128288bcab2f40f1f814d
Daniil <daniilminin1990@yandex.ru>
2023-11-06 21:38:08 +0600

    create header unstyled

---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header>
+      <a href="#">
+        <img src="" alt="" />
+      </a>
+      <nav>
+        <ul>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">Skills</a>
+          </li>
+          <li>
+            <a href="">Works</a>
+          </li>
+          <li>
+            <a href="">Testimony</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
