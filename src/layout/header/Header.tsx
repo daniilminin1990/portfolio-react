feb2974eebec733431137b0dd804b504583b1877
Daniil <daniilminin1990@yandex.ru>
2023-11-22 20:24:32 +0600

    Стилизовали Header, разъехжающиеся буквы

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 import { Logo } from "../../components/logo/Logo";
-import { Menu } from "../../components/menu/Menu";
 import { Container } from "../../components/Container";
 import { FlexWrapper } from "../../components/FlexWrapper";
+import { HeaderMenu } from "./headerMenu/HeaderMenu";
 
 const items = ["Home", "Skills", "Works", "Testimony", "Contact"];
 
@@ -13,7 +13,7 @@ export const Header = () => {
       <Container>
         <FlexWrapper justify={"space-between"} align={"center"}>
           <Logo />
-          <Menu menuItems={items} />
+          <HeaderMenu menuItems={items} />
         </FlexWrapper>
       </Container>
     </StyledHeader>
@@ -21,5 +21,11 @@ export const Header = () => {
 };
 
 const StyledHeader = styled.header`
-  background-color: #d4ffd3;
+  background-color: #1f1f20e5;
+  padding: 20px 0;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 99999;
 `;
