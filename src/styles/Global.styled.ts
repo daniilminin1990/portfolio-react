437b77833d3c14daa4b95fcbbbdef024d2a9c9b9
Daniil <daniilminin1990@yandex.ru>
2023-12-01 15:17:54 +0600

    Сделали фдаптив Skills

--- src/styles/Global.styled.ts
+++ src/styles/Global.styled.ts
@@ -38,6 +38,10 @@ export const GlobalStyle = createGlobalStyle`
 
   section {
     padding: 100px 0;
+
+    @media ${theme.media.mobile} {
+      padding: 80px 0;
+    }
   }
   section:nth-of-type(odd) {
     background-color: ${theme.colors.primaryBg};
