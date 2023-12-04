80cc446aba628c01b50e3b10ef445e4fcdedc0fe
Daniil <daniilminin1990@yandex.ru>
2023-12-04 20:29:47 +0600

    refactored Contacts

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -1,57 +1,21 @@
 import React from "react";
-import styled from "styled-components";
 import { SectionTitle } from "../../../components/SectionTitle";
 import { Button } from "../../../components/Button";
 import { Container } from "../../../components/Container";
-import { theme } from "../../../styles/Theme";
+import { S } from "./Contacts_Styles";
 
-export const Contacts = () => {
+export const Contacts: React.FC = () => {
   return (
-    <StyledContacts>
+    <S.Contacts>
       <Container>
         <SectionTitle>Contact</SectionTitle>
-        <StyledForm>
-          <Field placeholder={"Name"} />
-          <Field placeholder={"Subject"} />
-          <Field as={"textarea"} placeholder={"Message"} />
+        <S.Form>
+          <S.Field placeholder={"Name"} />
+          <S.Field placeholder={"Subject"} />
+          <S.Field as={"textarea"} placeholder={"Message"} />
           <Button type={"submit"}>Send message</Button>
-        </StyledForm>
+        </S.Form>
       </Container>
-    </StyledContacts>
+    </S.Contacts>
   );
 };
-
-const StyledContacts = styled.section``;
-const StyledForm = styled.form`
-  max-width: 540px;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 16px;
-  margin: 0 auto;
-
-  textarea {
-    resize: none;
-    height: 155px;
-  }
-`;
-const Field = styled.input`
-  width: 100%;
-  background-color: ${theme.colors.secondaryBg};
-  border: 1px solid ${theme.colors.borderColor};
-  padding: 7px 15px;
-  font-family: Poppins, sans-serif;
-  font-weight: 400;
-  font-size: 12px;
-  letter-spacing: 0.05em;
-  color: ${theme.colors.font};
-  &::placeholder {
-    color: ${theme.colors.placeholderColor};
-    text-transform: capitalize;
-  }
-
-  &:focus-visible {
-    outline: 1px solid ${theme.colors.borderColor};
-  }
-`;
